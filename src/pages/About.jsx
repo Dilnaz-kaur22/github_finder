b177fba81c11079d681fec56fc1b2e8424fb61c9
Dilnaz-kaur22 <126363154+Dilnaz-kaur22@users.noreply.github.com>
2023-05-20 02:07:02 -0500

    project created

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+
+const About = () => {
+    return (
+        <div>
+            <h1 className="text-5xl mb-4">Github Finder</h1>
+            <p className="mb-4 text-2xl font-light">
+                A react app that lets you search for github users.
+            </p>
+        </div>
+    )
+}
+
+export default About
